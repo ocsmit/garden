cc816a1dc44245ce2571234536a45a6442dd8d5a
ocsmit <owen8461@protonmail.com>
2020-06-05 14:03:37 -0400

    add color map option

--- garden.py
+++ garden.py
@@ -1,12 +1,12 @@
 import freenect
 import numpy as np
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 
-def save_ir_plot(out_plot=None):
+def save_ir_plot(out_plot=None, color_map='Spectral'):
     ir = freenect.sync_get_video(0, freenect.VIDEO_IR_10BIT)
-    plt.imshow(ir[0])
+    plt.imshow(ir[0], color_map)
     if out_plot:
-        plt.savefig(out_plot)
+        plt.savefig(out_plot, bbox_inches='tight')
     else:
         return ir[0]
 
