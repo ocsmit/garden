0e8843731b13a2a7159fecd0d6551af638a5db3c
ocsmit <owen8461@protonmail.com>
2020-06-05 18:17:27 -0400

    Format output file names
    Add time and date to file names with get_time(), output filenames are now formated automatically

--- garden.py
+++ garden.py
@@ -1,19 +1,33 @@
 import freenect
 import numpy as np
 from matplotlib import pyplot as plt
+from datetime import datetime
 
-def save_ir_plot(out_plot=None, color_map='Spectral'):
+
+def get_time():
+    now = datetime.now()
+    time = now.strftime("%H%M%S")
+    date = now.strftime("%m%d%Y")
+    return "%s_%s.png" % (time, date)
+
+def nir(outdir=None, color_map='Spectral'):
     ir = freenect.sync_get_video(0, freenect.VIDEO_IR_10BIT)
     plt.imshow(ir[0], color_map)
-    if out_plot:
-        plt.savefig(out_plot, bbox_inches='tight')
+    if outdir:
+        timeDate = get_time()
+        fileName = 'nir_%s' % timeDate
+        out = '%s/%s' % (outdir, fileName)
+        plt.savefig(out, bbox_inches='tight')
     else:
         return ir[0]
 
-def rgb(out_plot=None):
+def rgb(outdir=None):
     rgb = freenect.sync_get_video(0, freenect.VIDEO_RGB)
     plt.imshow(rgb[0])
-    if out_plot:
-        plt.savefig(out_plot, bbox_inches='tight')
+    if outdir:
+        timeDate = get_time()
+        fileName = 'rgb_%s' % timeDate
+        out = '%s/%s' % (outdir, fileName)
+        plt.savefig(out, bbox_inches='tight')
     else:
         return rgb[0]
