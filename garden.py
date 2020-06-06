d8088bbec3e94596d2f55fc7561f3cdd14a67806
ocsmit <owen8461@protonmail.com>
2020-06-05 23:06:29 -0400

    kinect_init | get_band

--- garden.py
+++ garden.py
@@ -3,6 +3,19 @@ import numpy as np
 from matplotlib import pyplot as plt
 from datetime import datetime
 
+def init_kinect(*args):
+    if args == 'on':
+        context = freenect.init()
+        kinect = freenect.open_device(context, 0)
+    if args == 'off':
+        freenect.close_device(kinect)
+        freenect.shutdown(kinect)
+
+def depth_switch(*args):
+    if args == 'on':
+        freenect.start_depth(depthSensor)
+    if args == 'off':
+        freenect.stop_depth(depthSensor)
 
 def get_time():
     now = datetime.now()
@@ -11,8 +24,11 @@ def get_time():
     return "%s_%s.png" % (time, date)
 
 def nir(outdir=None, color_map='Spectral'):
+    # init_kinect('on')
     ir = freenect.sync_get_video(0, freenect.VIDEO_IR_10BIT)
+    # init_kinect('off')
     plt.imshow(ir[0], color_map)
+    freenect.sync_stop()
     if outdir:
         timeDate = get_time()
         fileName = 'nir_%s' % timeDate
@@ -22,12 +38,22 @@ def nir(outdir=None, color_map='Spectral'):
         return ir[0]
 
 def rgb(outdir=None):
+    # init_kinect('on')
     rgb = freenect.sync_get_video(0, freenect.VIDEO_RGB)
-    plt.imshow(rgb[0])
+    # init_kinect('off')
     if outdir:
+        plt.imshow(rgb[0])
         timeDate = get_time()
         fileName = 'rgb_%s' % timeDate
         out = '%s/%s' % (outdir, fileName)
         plt.savefig(out, bbox_inches='tight')
     else:
         return rgb[0]
+
+def get_band(arr, band):
+    out_arr = []
+    for i in range(arr[0]):
+        for j in range(arr[1]):
+            out_arr.append(arr[i][j][band])
+    outArr = out_arr.reshape(arr.shape)
+
