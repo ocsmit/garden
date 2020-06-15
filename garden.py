ce8eabe2352b133014109f035a8192c4256f3382
ocsmit <owen8461@protonmail.com>
2020-06-15 19:12:32 -0400

    Change image saving to use imsave

--- garden.py
+++ garden.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env
+
 import freenect
 import numpy as np
 from matplotlib import pyplot as plt
 from datetime import datetime
+from PIL import Image
+
+out_dir = "/home/pi/garden_out"
 
 def init_kinect(*args):
     if args == 'on':
@@ -19,36 +24,29 @@ def depth_switch(*args):
 
 def get_time():
     now = datetime.now()
-    time = now.strftime("%H%M%S")
+    time = now.strftime("%H%M")
     date = now.strftime("%m%d%Y")
     return "%s_%s.png" % (time, date)
 
 def nir(outdir=None, color_map='Spectral'):
-    # init_kinect('on')
     ir = freenect.sync_get_video(0, freenect.VIDEO_IR_10BIT)
-    # init_kinect('off')
-    plt.imshow(ir[0], color_map)
     freenect.sync_stop()
-    if outdir:
-        timeDate = get_time()
-        fileName = 'nir_%s' % timeDate
-        out = '%s/%s' % (outdir, fileName)
-        plt.savefig(out, bbox_inches='tight')
-    else:
-        return ir[0]
+
+    timeDate = get_time()
+    fileName = 'nir_%s' % timeDate
+    out = '%s/%s' % (outdir, fileName)
+
+    plt.imsave(out, ir[0], format='png')
 
 def rgb(outdir=None):
-    # init_kinect('on')
     rgb = freenect.sync_get_video(0, freenect.VIDEO_RGB)
-    # init_kinect('off')
-    if outdir:
-        plt.imshow(rgb[0])
-        timeDate = get_time()
-        fileName = 'rgb_%s' % timeDate
-        out = '%s/%s' % (outdir, fileName)
-        plt.savefig(out, bbox_inches='tight')
-    else:
-        return rgb[0]
+    freenect.sync_stop()
+
+    timeDate = get_time()
+    fileName = 'rgb_%s' % timeDate
+    out = '%s/%s' % (outdir, fileName)
+
+    plt.imsave(out, rgb[0], format='png')
 
 def get_band(arr, band):
     out_arr = []
@@ -57,3 +55,8 @@ def get_band(arr, band):
             out_arr.append(arr[i][j][band])
     outArr = out_arr.reshape(arr.shape)
 
+if __name__ == "__main__":
+    init_kinect('on')
+    rgb(outdir=out_dir)
+    nir(outdir=out_dir)
+    init_kinect('off')
